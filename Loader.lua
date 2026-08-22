af706b4a3812ebdffb4e0e5c93f010cec47622ae
scripter.sm <faithsmiley.robloxscripts@gmail.com>
2026-08-22 21:35:46 +0500

    .

--- Loader.lua
+++ Loader.lua
@@ -13,4 +13,8 @@ if (getgenv().EZ_LOADED) then
 end;
 getgenv().EZ_LOADED = true;
 
+getgenv().EZ_VERSION = game:GetService("HttpService"):JSONDecode(
+	game:HttpGet("https://raw.githubusercontent.com/scripter-sm/EliteZone/main/Dependencies/Version.json")
+).version;
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/31283a1cc203e3f65710cffc683c3bee.lua"))()
