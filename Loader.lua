c09c34080a3610076c587a92bfb82097208f06c0
scripter.sm <faithsmiley.robloxscripts@gmail.com>
2026-08-24 08:53:07 +0500

    .

--- Loader.lua
+++ Loader.lua
@@ -9,12 +9,8 @@ _/ __ \\___   /
 ]]
 
 if (getgenv().EZ_LOADED) then
+	warn("[ Elite Zone ] Elite Zone is already loaded.")
 	return;
 end;
-getgenv().EZ_LOADED = true;
-
-getgenv().EZ_VERSION = game:GetService("HttpService"):JSONDecode(
-	game:HttpGet("https://raw.githubusercontent.com/scripter-sm/EliteZone/main/Dependencies/Version.json")
-).version;
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/31283a1cc203e3f65710cffc683c3bee.lua"))()
