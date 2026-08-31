609d2b3b1aeeb84ef08f67b57d0e325e4f735a6d
Scripter.SM <faithsmiley.robloxscripts@gmail.com>
2026-08-31 14:50:03 +0500

    build: auto-compile gui

--- Dependencies/libraries/gui.lua
+++ Dependencies/libraries/gui.lua
@@ -1807,7 +1807,7 @@ function EZ:LoadGUI()
 						label.BorderSizePixel = 0
 						label.FontFace = FontOption.Value
 						label.Position = UDim2.fromOffset(isRight and 5 or 9, 2)
-						label.Text = name..(module.ExtraText and " <font color="A8A8A8'>"..module.ExtraText()..'</font>' or '')
+						label.Text = name..(module.ExtraText and ' <font color="#A8A8A8">'..module.ExtraText()..'</font>' or '')
 						label.TextSize = 18
 						label.RichText = true
 		
