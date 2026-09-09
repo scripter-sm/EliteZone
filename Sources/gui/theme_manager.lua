585f39b1858557ef30b004728a977f69c0a2f979
Scripter.SM <faithsmiley.robloxscripts@gmail.com>
2026-09-09 20:03:42 +0500

    feat: zero background blur on default themes for mobile

--- Sources/gui/theme_manager.lua
+++ Sources/gui/theme_manager.lua
@@ -7,7 +7,7 @@ do
 	local NonColorDefaults = {
 		Background_Color = '000000';
 		Background_Transparency = 0.7;
-		Background_Blur = 15;
+		Background_Blur = EZ.IsMobile and 0 or 15;
 		Background_Contrast = 0;
 		Background_Saturation = 0;
 		Background_Brightness = 0;
